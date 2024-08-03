94c7d816cd4ae328b63a7ce82be38211b2af331a
Golam Muntakim <seams123456789@gmail.com>
2024-08-02 23:40:44 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+
+
+function App() {
+
+
+  return (
+    <>
+    <div className="flex max-w-[1200px]">
+      <div className="w-[400px]">
+        <h1 className=" text-4xl mx-auto text-blue-500 text-center mt-10">Logo</h1>
+       <div className="ml-[150px] mt-10">
+       <ul className="space-y-4">
+          <li>Home</li>
+          <li>New Listing</li>
+          <li>Search</li>
+          <li>About</li>
+          <li>Favourites</li>
+          <li>Help Center</li>
+          <li>Help Center</li>
+          <li>Settings</li>
+        </ul>
+       </div>
+        </div>
+      <div className="w-[800px]">d</div>
+    </div>
+    </>
+  )
+}
+
+export default App
